ee89363a002070dccdcaf484ca4260782b580030
Jonathan Willianto <jowillianto@gmail.com>
2023-06-23 23:05:52 +0900

    [#01] Added a more convenient parser, the new parser offers a better API for kwargs but a little more cumbersome for sequential arguments

---
+++ parser.cpp
@@ -0,0 +1,228 @@
+#include "parser.hpp"
+
+namespace ArgParse{
+  const char* ParserError::what() const noexcept{
+    return "Unknown parsing error";
+  }
+  InvalidKey::InvalidKey(const std::string& key){
+    _msg = "The following key \'" + key + "\' is invalid";
+  }
+  const char* InvalidKey::what() const noexcept{
+    return _msg.c_str();
+  }
+  GenericParserError::GenericParserError(std::string&& msg){
+    _msg = std::move(msg);
+  }
+  const char* GenericParserError::what() const noexcept{
+    return _msg.c_str();
+  }
+  OutOfBounds::OutOfBounds(size_t pos){
+    _msg = "The following position " + std::to_string(pos) + 
+      " is not in the parser";
+  }
+  OutOfBounds::OutOfBounds(const std::string& key){
+    _msg = "The following key \'" + key + "\' does not exist in the parser";
+  }
+  const char* OutOfBounds::what() const noexcept{
+    return _msg.c_str();
+  }
+  void ArgsData::setData(const std::string& data){
+    if (!_isInitialized) 
+      _isInitialized = true;
+    _data = data;
+  }
+  const std::string& ArgsData::getData() const{
+    return _data;
+  }
+  void ArgsData::appendData(const std::string& data){
+    _data = _data.size() == 0 ? data : _data + ARG_SEPARATOR + data;
+  }
+  std::vector<std::string> ArgsData::splitBySeparator(char sep) const{
+    // Copy Internal Data before using it
+    std::string data;
+    data.reserve(_data.size() + 1);
+    data.push_back(sep);
+    // Separate it
+    std::vector<std::string> separated;
+    size_t entryCount = std::count(_data.begin(), _data.end(), sep);
+    separated.reserve(entryCount + 1);
+    // Actually Split it
+    size_t begPointer = 0;
+    size_t endPointer = 0;
+    for(endPointer = 0; endPointer < _data.size(); endPointer++){
+      if (_data.at(endPointer) == sep){
+        separated.push_back(
+          std::string(_data.begin() + begPointer, _data.begin() + endPointer)
+        );
+        begPointer = endPointer;
+      }
+    }
+    return separated;
+  }
+  Args::Args(
+    std::string&& helpString, bool required, bool many, 
+    std::string&& defaultValue
+  ){
+    _helpString = std::move(helpString);
+    _default    = std::move(defaultValue);
+    _required   = required;
+    _many       = many;
+  }
+  const std::string& Args::data() const{
+    return _data.getData();
+  }
+  const std::string& Args::defaultValue() const{
+    return _default;
+  }
+  const std::string& Args::helpString() const{
+    return _helpString;
+  }
+  bool Args::isInitialized() const{
+    return _data.getData().size() != 0;
+  }
+  bool Args::isMultiple() const{
+    return _many;
+  }
+  bool Args::isRequired() const{
+    return _required;
+  }
+  void Args::appendOrSetData(const std::string& data){
+    if(isMultiple()) 
+      _data.appendData(data);
+    else
+      _data.setData(data);
+  }
+  bool Parser::doesKeyExist(const Key& key) const{
+    return _kwargs.count(key) != 0;
+  }
+  void Parser::_keyExistOrException(const Key& key) const{
+    if (!doesKeyExist(key))
+      throw OutOfBounds(key);
+  }
+  bool Parser::doesPosExist(size_t pos) const{
+    return pos < _args.size();
+  }
+  void Parser::_posExistOrException(size_t pos) const{
+    if (!doesPosExist(pos))
+      throw OutOfBounds(pos);
+  }
+  bool Parser::isValidKey(const Key& key){
+    return key != "-h" && key != "--help" && isKwargTag(key);
+  }
+  bool Parser::validateKey(const std::string& key) const{
+    return isValidKey(key) && !doesKeyExist(key);
+  }
+  std::string Parser::_keyNameFromKey(const std::string& key) const{
+    if (key.length() == 2) 
+      return std::string(key.begin() + 1, key.end());
+    else if (key.length() > 2)
+      return std::string(key.begin() + 2, key.end());
+    else
+      throw InvalidKey(key);
+  }
+  const std::string& Parser::get(size_t pos) const{
+    _posExistOrException(pos);
+    return _args.at(pos).data();
+  }
+  const std::string& Parser::get(const std::string& key) const{
+    _keyExistOrException(key);
+    return _kwargs.at(key).data();
+  }
+  void Parser::getHelpString(std::ostream& stream) const{
+    stream << "Ordered Arguments List : \n";
+    for (const auto& entry : _args){
+      stream << "\t" << entry.helpString() << "\n";
+    }
+    stream << "Keyword Arguments List : \n";
+    for(const auto& [key, entry] : _kwargs){
+      if (key.length() == 1) stream << "\t-" << key;
+      else stream << "\t--" << key;
+      stream << "\t\t : "<< entry.helpString() << std::endl;
+    }
+  }
+  void Parser::parse(
+    int argc, char** argv, bool exitOnFail, bool printHelp
+  ){
+    try{
+      _parse(argc, argv);
+    }
+    catch(const ParserError& e){
+      if (exitOnFail){
+        std::cerr<< e.what() << std::endl;
+        if (printHelp) getHelpString(std::cerr);
+        exit(1);
+      }
+      else{
+        if (printHelp) getHelpString(std::cerr);
+        throw e;
+      }
+    }
+    catch(const PrintHelp& e){
+      getHelpString(std::cerr);
+      exit(1);
+    }
+  }
+  void Parser::checkForHelpArgv(int argc, char** argv){
+    for(size_t i = 1; i < argc; i++){
+      if (argv[i] == std::string("-h") || argv[i] == std::string("--help"))
+        throw PrintHelp();
+    }
+  }
+  bool Parser::isKwargTag(const std::string& key){
+    return (
+      key.length() == 2 && key.at(0) == '-'
+    ) || (
+      key.length() > 2 && key.substr(0, 2) == "--"
+    );
+  }
+  void Parser::_parseArgv(int argc, char** argv){
+    size_t argCount = 0;
+    bool haveKey = false;
+    std::string curArgKey = "";
+    for(size_t i = 1; i < argc; i++){
+      std::string arg = argv[i];
+      bool isKwargStart = isKwargTag(arg);
+      if (isKwargStart && !haveKey){
+        if (!doesKeyExist(_keyNameFromKey(arg))){
+          throw OutOfBounds(arg);
+        }
+        curArgKey = _keyNameFromKey(arg);
+        haveKey = true;
+      }
+      else if (!isKwargStart && haveKey){
+        _kwargs.at(curArgKey).appendOrSetData(arg);
+        haveKey = false;
+      }
+      else if (!isKwargStart && !haveKey){
+        if (argCount > _args.size())
+          throw OutOfBounds(argCount);
+        _args.at(argCount).appendOrSetData(arg);
+        argCount += 1;
+      }
+      else
+        throw ParserError();
+    }
+    if (haveKey){
+      throw GenericParserError("The key \'" + curArgKey + " is uninitialized");
+    }
+    size_t errCount = 0;
+    for(auto& entry : _args){
+      if (!entry.isInitialized())
+        entry.appendOrSetData(entry.defaultValue());
+      if (entry.isRequired() && !entry.isInitialized())
+        throw GenericParserError("Ordered Arguments are incomplete");
+    }
+    for(auto& [key, entry] : _kwargs){
+      if (!entry.isInitialized())
+        entry.appendOrSetData(entry.defaultValue());
+      if (entry.isRequired() && !entry.isInitialized())
+        throw GenericParserError(
+          "Inordered Argument \'" + key + "\' is not given"
+        );
+    }
+  }
+  void Parser::_parse(int argc, char** argv){
+    checkForHelpArgv(argc, argv);
+    _parseArgv(argc, argv);
+  }
+};
--- parser.hpp
+++ parser.hpp
@@ -1,142 +1,299 @@
 #pragma once
+#include <string>
+#include <functional>
 #include <vector>
 #include <map>
+#include <exception>
 #include <iostream>
-#include <string>
-#include <memory>
+
+#define ARG_SEPARATOR ','
 
 namespace ArgParse{
-  // Helper Class that is subject to change
+  class ParserError : public std::exception{
+    public:
+      virtual const char* what() const noexcept;
+  };
+  class GenericParserError : public ParserError{
+    public:
+      GenericParserError(std::string&& msg);
+      const char* what() const noexcept;
+    private:
+      std::string _msg;
+  };
+  class PrintHelp{};
+  class InvalidKey : public ParserError{
+    public:
+      InvalidKey(const std::string& key);
+      const char* what() const noexcept;
+    private:
+      std::string _msg;
+  };
+  class OutOfBounds : public ParserError{
+    public:
+      OutOfBounds(size_t pos);
+      OutOfBounds(const std::string& key);
+      const char* what() const noexcept;
+    private:
+      std::string _msg;
+  };
+  class ArgsData{
+    public:
+      void setData(const std::string& data);
+      const std::string& getData() const;
+      void appendData(const std::string& data);
+      std::vector<std::string> splitBySeparator(char sep) const;
+    private:
+      std::string _data;
+      bool _isInitialized = false;
+  };
   class Args{
     public:
       Args(
-        std::string&& helpString = "",
-        bool required = true,
-        bool many = false
-      ){
-        _helpString = std::move(helpString);
-        _bulk       = many;
-        _required   = required;
-      }
-      const std::string& data() const{
-        return _data[0];
-      }
-      const std::vector<std::string>& allData() const{
-        return _data;
-      }
-    private:
-      bool _bulk;
-      bool _required;
-      std::vector<std::string> _data;
-      std::string _helpString;
+        std::string&& helpString = "", bool required = true, bool many = false, 
+        std::string&& defaultValue = ""
+      );
+      const std::string& data() const;
+      const std::string& defaultValue() const;
+      const std::string& helpString() const;
+      bool isMultiple() const;
+      bool isRequired() const;
+      bool isInitialized() const;
+      void appendOrSetData(const std::string& data);
 
-      friend class Parser;
-  };
-  class Parser{
-    public:
-      void printHelp() const{
-        std::cout<<"Ordered Arguments List : "<<std::endl;
-        for(const auto& entry : _args)
-          std::cout<<"\t"<<entry._helpString<<std::endl;
-        std::cout<<"Keyword arguments List : "<<std::endl;
-        for(const auto& [key, entry] : _kwargs)
-          std::cout<<"\t--"<<key<<"\t\t:"<<entry._helpString<<std::endl;
-      }
-      void addArgument(Args&& args){
-        _args.emplace_back(args);
-      }
-      void addArgument(const std::string& name, Args&& args){
-        if (_kwargs.count(name) != 0)
-          throw "Key already exists";
-        if (name == "h")
-          throw "h is a forbidden name";
-        if (name.find("-") != std::string::npos)
-          throw "- should not appear in the name";
-        // Add into the map
-        _kwargs.emplace(name, args);
-      }
-      void parse(int argc, char** argv){
-        // Check for -h and print help if exists
-        for(size_t i = 1; i < argc; i++){
-          if(argv[i] == std::string("-h")){
-            printHelp();
-            return;
-          }
-        }
-        // Real Parsing
-        size_t argCount   = 0;
-        bool haveKey      = false;
-        std::string curArg= "";
-        for (size_t i = 1; i < argc; i++){
-          std::string arg(argv[i]);
-          bool isKwargStart = arg.substr(0, 2) == "--";
-          if (isKwargStart && !haveKey){
-            if (_kwargs.count(arg.substr(2)) == 0){
-              std::cerr<<"No such key "<< arg << std::endl;
-              std::cerr<<"Call " << argv[0] << " -h for help"<<std::endl;
-              throw "Error Parsing";
-            }
-            curArg  = arg.substr(2);
-            haveKey = true;
-          }
-          // Add Kwargs Argument
-          else if (!isKwargStart && haveKey){
-            auto& args  = _kwargs[curArg];
-            if (!args._bulk && args._data.size() != 0)
-              throw curArg + " have too many arguments";
-            args._data.push_back(argv[i]);
-            haveKey = false;
-          }
-          // Add Args Argument
-          else if(!isKwargStart && !haveKey){
-            if (argCount > _args.size())
-              throw "Too many ordered arguments";
-            _args[argCount]._data.push_back(argv[i]);
-            argCount += 1;
-          }
-          else
-            throw "Error Parsing";
-        }
-        if (haveKey){
-          std::cerr<<"Key " << curArg << " have no value"<<std::endl;
-          std::cerr<<"Call "<< argv[0] << " -h for help"<<std::endl;
-          throw "Error Parsing";
-        }
-        size_t errCount = 0;
-        // Check requireds
-        for (const auto& entry : _args){
-          if (entry._required && entry._data.size() == 0){
-            std::cerr<<"Ordered Arguments are incomplete";
-            errCount += 1;
-          }
-        }
-        for (const auto& [key, entry] : _kwargs){
-          if (entry._required && entry._data.size() == 0){
-            std::cerr<<"Named Arguments for key " << key <<" have not been given"<<std::endl;
-            errCount += 1;
-          }
-        }
-        if (errCount > 0) 
-          throw "Error Parsing";
-        _parsed = true;
-      }
-      const Args& getKwargs(const std::string& key) const{
-        if (_parsed == false){
-          throw "Parsing have not been done";
-        }
-        if (_kwargs.count(key) == 0)
-          throw "No such name";
-        return _kwargs.at(key);
-      }
-      const Args& getArgs(size_t key) const{
-        if (_parsed == false){
-          throw "Parsing have not been done";
-        }
-        return _args.at(key);
-      }
+      // Split out
+      template<typename Target>
+      std::vector<Target> convert(char sep) const;
+      template<typename Target>
+      Target convert() const;
     private:
-      std::map<std::string, Args> _kwargs;
-      std::vector<Args> _args;
-      bool _parsed = false;
+      std::string _helpString;
+      std::string _default;
+      ArgsData _data;
+      bool _required;
+      bool _many;
   };
-};
+
+  class Parser{
+    using Key = std::string;
+    public:
+      /* Adding arguments */
+      template<typename... T>
+      /**
+       * @brief adds a sequential argument to the parser
+      */
+      void addSequentialArgument(T&& ...args);
+      template<typename... T>
+      /**
+       * @brief adds a keyed argument to the parser
+       * @param key the key to use, including "--", for example : "--file", 
+       * this decision to include "--" is purely for ease of use
+      */
+      void addArgument(Key&& key, T&& ...args);
+      void getHelpString(std::ostream& stream) const;
+      /**
+       * @brief Parses the contents of argv into the parser
+       * @param argc argument count received from the main function
+       * @param argv argument values received from the main function
+       * @param exitOrException if the function should exit immediately or throw
+       * and exception when any parsing error happens. True is to exit directly
+       * @param printHelp if the help message should be printed when any error 
+       * is encountered before the function exits or an exception is thrown. If
+       * --help or -h is invoked this option will be ignored and the help will
+       * be printed anyway and exit will be invoked.
+      */
+      void parse(
+        int argc, char** argv, bool exitOrException = true,bool printHelp = true
+      );
+      
+      /*
+        Obtaining the content with conversion
+      */
+      const std::string& get(size_t pos) const;
+      const std::string& get(const Key& key) const;
+      template<typename T>
+      T get(size_t pos) const;
+      template<typename T>
+      T get(const Key& key) const;
+      template<typename T>
+      std::vector<T> get(size_t pos, char sep) const;
+      template<typename T>
+      std::vector<T> get(const Key& key, char sep) const;
+
+      /*
+        Helper Functions
+      */
+      static bool isValidKey(const Key& key);
+      static void checkForHelpArgv(int argc, char** argv);
+      static bool isKwargTag(const std::string& s);
+
+      /* 
+        Non Static Helper
+      */
+      bool validateKey(const Key& key) const;
+      bool doesKeyExist(const Key& key) const;
+      bool doesPosExist(size_t pos) const;
+      
+    private:
+      std::map<Key, Args> _kwargs;
+      std::vector<Args> _args;
+
+      void _parse(int argc, char** argv);
+      void _keyExistOrException(const Key& key) const;
+      void _posExistOrException(size_t pos) const;
+      Key _keyNameFromKey(const Key& key) const;
+      void _parseArgv(int argc, char** argv);
+
+  };
+  template<typename T>
+  inline T Args::convert() const{
+    return T(_data.getData());
+  }
+  template<typename T>
+  inline std::vector<T> Args::convert(char sep) const{
+    std::vector<T> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){ return T(s);}
+    );
+    return converted;
+  }
+
+  // Parser
+  template<typename... T>
+  inline void Parser::addSequentialArgument(T&& ...args){
+    _args.emplace_back(std::forward<T>(args)...);
+  }
+  template<typename ...T>
+  inline void Parser::addArgument(Key&& key, T&& ...args){
+    validateKey(key);
+    _kwargs.emplace(_keyNameFromKey(key), Args(std::forward<T>(args)...));
+  }
+  template<typename T>
+  inline T Parser::get(size_t pos) const{
+    _posExistOrException(pos);
+    return _args.at(pos).convert<T>();
+  }
+  template<typename T>
+  inline T Parser::get(const std::string& key) const{
+    _keyExistOrException(key);
+    return _kwargs.at(key).convert<T>();
+  }
+  template<typename T>
+  inline std::vector<T> Parser::get(size_t pos, char sep) const{
+    _posExistOrException(pos);
+    const Args& arg = _args.at(pos);
+    return arg.convert<T>(sep);
+  }
+  template<typename T>
+  inline std::vector<T> Parser::get(const Key& key, char sep) const{
+    _keyExistOrException(key);
+    const Args& arg = _kwargs.at(key);
+    return arg.convert<T>(sep);
+  }
+  template<>
+  inline double Args::convert<double>() const{
+    return std::atof(_data.getData().c_str());
+  }
+  template<>
+  inline int Args::convert<int>() const{
+    return std::atoi(_data.getData().c_str());
+  }
+  template<>
+  inline bool Args::convert<bool>() const{
+    char firstCharacter = _data.getData().at(0);
+    return firstCharacter == 't' || firstCharacter == 'T';
+  }
+  template<>
+  inline uint32_t Args::convert<uint32_t>() const{
+    return static_cast<uint32_t>(convert<int>());
+  }
+  template<>
+  inline size_t Args::convert<size_t>() const{
+    return static_cast<size_t>(convert<int>());
+  }
+  template<>
+  inline float Args::convert<float>() const{
+    return static_cast<float>(convert<double>());
+  }
+  template<>
+  inline std::vector<double> Args::convert<double>(char sep) const{
+    std::vector<double> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        return std::atof(s.c_str());
+      }
+    );
+    return converted;
+  }
+  template<>
+  inline std::vector<int> Args::convert<int>(char sep) const{
+    std::vector<int> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        return std::atoi(s.c_str());
+      }
+    );
+    return converted;
+  }
+  template<>
+  inline std::vector<bool> Args::convert<bool>(char sep) const{
+    std::vector<bool> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        char firstCharacter = s.at(0);
+        return firstCharacter == 't' || firstCharacter == 'T';
+      }
+    );
+    return converted;
+  }
+  template<>
+  inline std::vector<uint32_t> Args::convert<uint32_t>(char sep) const{
+    std::vector<uint32_t> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        return static_cast<uint32_t>(std::atoi(s.c_str()));
+      }
+    );
+    return converted;
+  }
+  template<>
+  inline std::vector<size_t> Args::convert<size_t>(char sep) const{
+    std::vector<size_t> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        return static_cast<size_t>(std::atoi(s.c_str()));
+      }
+    );
+    return converted;
+  }
+  template<>
+  inline std::vector<float> Args::convert<float>(char sep) const{
+    std::vector<float> converted;
+    auto separated = _data.splitBySeparator(sep);
+    converted.reserve(separated.size());
+    std::transform(
+      separated.begin(), separated.end(), std::back_inserter(converted), 
+      [](const std::string& s){
+        return static_cast<float>(std::atof(s.c_str()));
+      }
+    );
+    return converted;
+  }
+}
